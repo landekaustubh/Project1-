c1b4acf75d0440f3fa6fe12631638e0e12a25996
landekaustubh <kaustubhlande98@gmail.com>
2025-12-07 19:27:20 +0530

    First commit

---
+++ src/com/velocity/ArrayListDemo.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList <Integer> al = new ArrayList<>();
+		
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		
+		System.out.println(al);
+
+	}
+
+}
